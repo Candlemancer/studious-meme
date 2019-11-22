7e0b9356daa3daa4830e01b5a12d239de85eb381
Suzie LaBarge <magicqwertyuiop@gmail.com>
2019-11-21 22:02:31 -0700

    got the axes of the big scatterplot done

--- scripts/monsters.js
+++ scripts/monsters.js
@@ -127,21 +127,26 @@ class Monsters {
 
     updateCharts() {
         let changedValue = d3.event.target.value;
+        console.log(changedValue);
         let xAxisValue = document.getElementById('xAxisSelect').value;
         let yAxisValue = document.getElementById('yAxisSelect').value;
+        let xAxisHeight = 20;
+        let yAxisWidth = 20;
         let partitionValue = document.getElementById('pAxisSelect').value;
         if (this.axisOptions.map(v => v.json).includes(changedValue)) {
             // not changing partition, big chart needs updated as well
             let bigGraphSVG = d3.select('#bigGraphSVG');
-            let bigXAxis = d3.scaleLinear()
-                .range([0, (this.svgBounds / 2) - this.margin.left - this.margin.right])
+            // remove what's there so we can make the new chart
+            document.getElementById('bigGraphSVG').innerHTML = '';
+            let bigXScale = d3.scaleLinear()
+                .range([0, (this.svgBounds.width / 2) - this.margin.left - this.margin.right])
                 .domain([d3.min(
                     this.monsters.map(m => m.attributes[xAxisValue])
                 )-1, d3.max(
                     this.monsters.map(m => m.attributes[xAxisValue])
                 )+1])
             ;
-            let bigYAxis = d3.scaleLinear()
+            let bigYScale = d3.scaleLinear()
                 .range([0, (this.svgBounds.height) - this.margin.top - this.margin.bottom])
                 .domain([d3.min(
                     this.monsters.map(m => m.attributes[yAxisValue])
@@ -150,9 +155,34 @@ class Monsters {
                 )+1])
             ;
             // add x-axis
+            let bigXAxis = d3.axisBottom(bigXScale)
+                .tickFormat((d,i) => d)
+            ;
             bigGraphSVG.append('g')
                 .attr('class', 'x-axis')
-                .attr('transform', 'translate')
+                .attr('transform', `translate(${yAxisWidth}, ${this.svgBounds.height-xAxisHeight})`)
+                .call(bigXAxis)
+                .selectAll('text')
+                .style('text-anchor', 'end')
+                .attr('dx', '-.8em')
+                .attr('dy', '.15em')
+                .attr('transform', 'rotate(-65)');
+            ;
+            // add y-axis
+            let bigYAxis = d3.axisLeft(bigYScale)
+                .tickFormat((d,i) => d)
+            ;
+            bigGraphSVG.append('g')
+                .attr('class', 'y-axis')
+                .attr('transform', `translate(${yAxisWidth}, ${this.svgBounds.height-xAxisHeight}) scale(1,-1)`)
+                .call(bigYAxis)
+                .selectAll('text')
+                .style('text-anchor', 'end')
+                .attr('dx', '0em')
+                .attr('dy', '.5em')
+                .attr('transform', 'scale(1,-1)')
+            ;
+            // k, I got the axes working and it's late enough I'll put the other stuff in tomorrow
         }
         // always update the small multiples
     }
