5ee11a685121edefa7a474a29d087dc549b6d7a5
Suzie LaBarge <magicqwertyuiop@gmail.com>
2019-11-22 22:11:48 -0700

    made it so things run post-merge

--- scripts/script.js
+++ scripts/script.js
@@ -12,7 +12,7 @@ function simulateBattle() {
     let playerRoster = party.getPartyMembers().map(p => new Entity(p))
 
     const NUM_MONSTERS = 10;
-    var randomMonsters = [];
+    var monsterRoster = [];
     for (var i = 0; i < NUM_MONSTERS; ++i)
     {
         let index = Math.floor(Math.random() * 799);
@@ -20,7 +20,7 @@ function simulateBattle() {
         monsterRoster.push(monsters.monsters[index]);
         console.log("added monster", index, ":", monsters.selectedMonsters[index]);
     }
-    monsterRoster = randomMonsters.map(m => new Entity(m));
+    monsterRoster = monsterRoster.map(m => new Entity(m));
 
     // console.log("Preparing fight between ", playerRoster, " and ", monsterRoster);
 
