b746716cc0408357f7a515f6446f660dee54cd7f
Jonathan Petersen <Candlemancer@gmail.com>
2019-11-22 18:41:16 -0700

    Basic Simulations

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,33 @@
 let party = new Party();
+let monsterList;
+
 d3.json('data/monsters.json').then(function(data){
-    let monsters = new Monsters(data);
+    monsterList = new Monsters(data);
 });
+
+party.addDefaultParty();
+
+function simulateBattle() {
+
+    let playerRoster = party.getPartyMembers().map(p => new Entity(p))
+
+    var monsterRoster = [];
+    for (var i = 0; i < 5; ++i)
+    {
+        let index = Math.floor(Math.random() * 799);
+        monsterRoster.push(monsterList.monsters[index]);
+        console.log("added monster", index, ":", monsterList[index]);
+    }
+    monsterRoster = monsterRoster.map(m => new Entity(m));
+
+    console.log("Preparing fight between ", playerRoster, " and ", monsterRoster);
+
+    var sim = new simulator(simPanel, playerRoster, monsterRoster);
+    sim.simulate(500);
+
+    simPanel.updatePlayerData(playerRoster);
+    simPanel.updateMonsterData(monsterRoster);
+
+}
+
+let simPanel = new SimulationPanel();
